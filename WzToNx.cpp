72271636feee4f2d88e8f444a3ca0ef3e4ecb016
retep998 <retep998@verizon.net>
2013-05-11 20:39:31 -0400

    Even more major progress
    Signed-off-by: retep998 <retep998@verizon.net>

--- WzToNx.cpp
+++ WzToNx.cpp
@@ -1,13 +1,12 @@
 #include <Windows.h>
 #include <filesystem>
 #include <iostream>
-#include <cassert>
 #include <codecvt>
 #include <vector>
 #include <queue>
+#include <map>
 using namespace std;
 using namespace std::tr2::sys;
-#include "Keys.h"
 
 enum class Type : uint16_t {
     none = 0,
@@ -41,12 +40,22 @@ struct Node {
         };
     };
 };
+struct String {
+    uint16_t size;
+    char * data;
+};
+
+map<uint64_t, uint32_t> StringSet;
+vector<String> Strings;
 vector<Node> Nodes;
 queue<uint32_t> Directories;
 queue<pair<uint32_t, uint32_t>> Imgs;
 char16_t WSBuf[0x8000];
 char SBuf[0x10000];
 codecvt_utf8<char16_t> Conv;
+extern uint8_t BMSKey[65536];
+extern uint8_t GMSKey[65536];
+extern uint8_t KMSKey[65536];
 uint8_t * Keys[3] = {BMSKey, GMSKey, KMSKey};
 uint8_t * Key;
 uint32_t FileStart;
@@ -58,15 +67,35 @@ void * OutFileHandle;
 void * OutMapHandle;
 char * OutBase;
 char * OutOffset;
+char * AllocBuffer = nullptr;
+uint64_t AllocRemain = 0;
 
+void die(string s) {
+    cerr << s << endl;
+    throw;
+}
+uint32_t AddNode() {
+    Nodes.emplace_back();
+    return static_cast<uint32_t>(Nodes.size() - 1);
+}
+void * CheapAlloc(uint64_t size) {
+    if (size > AllocRemain) {
+        AllocBuffer = reinterpret_cast<char *>(VirtualAlloc(nullptr, 0x1000000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
+        AllocRemain = 0x1000000;
+    }
+    AllocRemain -= size;
+    void * v = AllocBuffer;
+    AllocBuffer += size;
+    return v;
+}
 void OpenInput(path p) {
-    assert(is_regular_file(p));
+    if (!is_regular_file(p)) die(p.string() + " is not a valid file");
     InFileHandle = CreateFileA(p.string().c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, nullptr);
-    assert(InFileHandle != INVALID_HANDLE_VALUE);
-    InMapHandle= CreateFileMappingA(InFileHandle, nullptr, PAGE_READONLY, 0, 0, nullptr);
-    assert(InMapHandle != nullptr);
+    if (InFileHandle == INVALID_HANDLE_VALUE) die("Failed to open input file handle");
+    InMapHandle = CreateFileMappingA(InFileHandle, nullptr, PAGE_READONLY, 0, 0, nullptr);
+    if (InMapHandle == nullptr) die("Failed to create a input file mapping");
     InBase = reinterpret_cast<char *>(MapViewOfFile(InMapHandle, FILE_MAP_READ, 0, 0, 0));
-    assert(InBase != nullptr);
+    if (InBase == nullptr) die("Failed to map a view of the input file");
     InOffset = InBase;
 }
 void CloseInput() {
@@ -75,13 +104,12 @@ void CloseInput() {
     CloseHandle(InFileHandle);
 }
 void OpenOutput(path p, uint64_t s) {
-    assert(is_regular_file(p));
     OutFileHandle = CreateFileA(p.string().c_str(), GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, 0, nullptr);
-    assert(OutFileHandle != INVALID_HANDLE_VALUE);
+    if (OutFileHandle == INVALID_HANDLE_VALUE) die("Failed to open output file handle");
     OutMapHandle = CreateFileMappingA(OutFileHandle, nullptr, PAGE_READWRITE, s >> 32, s & 0xffffffff, nullptr);
-    assert(OutMapHandle != nullptr);
+    if (OutMapHandle == nullptr) die("Failed to create a output file mapping");
     OutBase = reinterpret_cast<char *>(MapViewOfFile(OutMapHandle, FILE_MAP_ALL_ACCESS, 0, 0, 0));
-    assert(OutBase != nullptr);
+    if (OutBase == nullptr) die("Failed to map a view of the output file");
     OutOffset = OutBase;
 }
 void CloseOutput() {
@@ -117,12 +145,29 @@ int32_t ReadCInt() {
     if (a != -128) return a;
     return Read<int32_t>();
 }
+__declspec(noinline)
+uint32_t AddString(char const * data, uint16_t size) {
+    uint64_t v = 14695981039346656037ULL;
+    char const * s = data;
+	for (size_t i = size; i; --i, ++s) {
+		v ^= static_cast<uint64_t>(*s);
+		v *= 1099511628211ULL;
+	}
+	v ^= v >> 32;
+    auto it = StringSet.insert(pair<uint64_t, uint32_t>(v, static_cast<uint32_t>(Strings.size())));
+    if (it.second) {
+        String s = {size, reinterpret_cast<char *>(CheapAlloc(size))};
+        memcpy(s.data, data, size);
+        Strings.push_back(s);
+    }
+    return it.first->second;
+}
+__declspec(noinline)
 uint32_t ReadEncString() {
     int32_t len = Read<int8_t>();
-    assert(len != 0);
+    if (len == 0) return 0;
     if (len > 0) {
         if (len == 127) len = Read<int32_t>();
-        assert(len > 0);
         char16_t * ows = reinterpret_cast<char16_t *>(InOffset);
         InSkip(len * 2);
         char16_t * ws = WSBuf;
@@ -135,11 +180,10 @@ uint32_t ReadEncString() {
         const char16_t * fnext;
         char * tnext;
         Conv.out(state, WSBuf, WSBuf + len, fnext, SBuf, SBuf + 0x10000, tnext);
-        cout.write(SBuf, tnext - SBuf).put('\n');
+        len = static_cast<int32_t>(tnext - SBuf);
     } else {
         if (len == -128) len = Read<int32_t>();
         else len = -len;
-        assert(len > 0);
         char * os = InOffset;
         InSkip(len);
         char * s = SBuf;
@@ -148,11 +192,11 @@ uint32_t ReadEncString() {
         for (size_t i = len; i; --i, ++mask, ++os, ++s, ++key) {
             *s = *os ^ *key ^ mask;
         }
-        cout.write(SBuf, len).put('\n');
     }
-    return 0;
+    return AddString(SBuf, len);
 }
-uint32_t ReadPropString(uint32_t offset) {
+__declspec(noinline)
+uint32_t ReadPropString(uint64_t offset) {
     uint8_t a = Read<uint8_t>();
     switch (a) {
     case 0x00:
@@ -161,7 +205,7 @@ uint32_t ReadPropString(uint32_t offset) {
     case 0x01:
     case 0x1B:
         {
-            uint32_t o = Read<uint32_t>() + offset;
+            uint64_t o = Read<uint32_t>() + offset;
             uint64_t p = InTell();
             InSeek(o);
             uint32_t s = ReadEncString();
@@ -169,16 +213,15 @@ uint32_t ReadPropString(uint32_t offset) {
             return s;
         }
     default:
-        assert(false);
+        die("Unknown property string type: " + to_string(a));
     }
     return 0;
 }
+__declspec(noinline)
 void DeduceKey() {
     int32_t len = Read<int8_t>();
-    assert(len < 0);
     if (len == -128) len = Read<int32_t>();
     else len = -len;
-    assert(len > 0);
     Key = nullptr;
     for (uint8_t * key : Keys ) {
         char * os = InOffset;
@@ -191,21 +234,22 @@ void DeduceKey() {
         }
         if (valid) Key = key;
     }
-    assert(Key);
+    if (!Key) die("Failed to identify the locale");
     InSkip(len);
 }
+__declspec(noinline)
 void Directory(uint32_t node) {
     Node & n = Nodes[node];
     int32_t count = ReadCInt();
     n.num = count;
     n.children = static_cast<uint32_t>(Nodes.size());
-    for (int i = count; i; --i) {
+    for (int i = 0; i < count; ++i) {
         Nodes.emplace_back();
         Node & nn = Nodes.back();
         uint8_t type = Read<uint8_t>();
         switch (type) {
         case 1:
-            assert(false);
+            die("Found the elusive type 1 directory");
             break;
         case 2:{
             int32_t s = Read<int32_t>();
@@ -220,7 +264,7 @@ void Directory(uint32_t node) {
             nn.name = ReadEncString();
             break;
         default:
-            assert(false);
+            die("Unknown directory type");
         }
         uint32_t size = ReadCInt();
         ReadCInt();
@@ -229,15 +273,104 @@ void Directory(uint32_t node) {
         if (type == 4) Imgs.push(pair<uint32_t, uint32_t>(static_cast<uint32_t>(Nodes.size() - 1), size));
     }
 }
-void Img(pair<uint32_t, uint32_t> info) {
-    uint64_t p = InTell();
-    Node & n = Nodes[info.first];
-    InSeek(p + info.second);
+void SubProperty(uint32_t node, uint64_t offset);
+__declspec(noinline)
+void ExtendedProperty(uint32_t node, uint64_t offset) {
+    Node & n = Nodes[node];
+    uint32_t s = ReadPropString(offset);
+    String st = Strings[s];
+    string str(st.data, st.size);
+    if (str == "Property") {
+        InSkip(2);
+        SubProperty(node, offset);
+    } else if (str == "Canvas") {
+        InSkip(1);
+        if (Read<uint8_t>() == 1) {
+            InSkip(2);
+            SubProperty(node, offset);
+        }
+        //Canvas stuff
+    } else if (str == "Shape2D#Vector2D") {
+        n.type = Type::vector;
+        n.vector[0] = ReadCInt();
+        n.vector[1] = ReadCInt();
+    } else if (str == "Shape2D#Convex2D") {
+        int32_t ec = ReadCInt();
+        for (int i = 0; i < ec; ++i) {
+            uint32_t ni = AddNode();
+            Node & nn = Nodes[ni];
+            string es = to_string(i);
+            nn.name = AddString(es.c_str(), static_cast<uint16_t>(es.size()));
+            ExtendedProperty(ni, offset);
+        }
+    } else if (str == "Sound_DX8") {
+        //Audio stuff
+    } else if (str == "UOL") {
+        n.type = Type::uol;
+        n.string = ReadPropString(offset);
+    } else die("Unknown ExtendedProperty type");
 }
+__declspec(noinline)
+void SubProperty(uint32_t node, uint64_t offset) {
+    Node & n = Nodes[node];
+    int32_t count = ReadCInt();
+    n.num = count;
+    n.children = static_cast<uint32_t>(Nodes.size());
+    for (int i = 0; i < count; ++i) {
+        uint32_t ni = AddNode();
+        Node & nn = Nodes[ni];
+        nn.name = ReadPropString(offset);
+        uint8_t type = Read<uint8_t>();
+        switch (type) {
+        case 0x00:
+            nn.type = Type::ireal;
+            nn.ireal = i;
+            break;
+        case 0x0B:
+        case 0x02:
+            nn.type = Type::ireal;
+            nn.ireal = Read<uint16_t>();
+            break;
+        case 0x03:
+            nn.type = Type::ireal;
+            nn.ireal = ReadCInt();
+            break;
+        case 0x04:
+            nn.type = Type::dreal;
+            if (Read<uint8_t>() == 0x80) nn.dreal = Read<float>();
+            else nn.dreal = 0.f;
+            break;
+        case 0x05:
+            nn.type = Type::dreal;
+            nn.dreal = Read<double>();
+            break;
+        case 0x08:
+            nn.type = Type::string;
+            nn.string = ReadPropString(offset);
+            break;
+        case 0x09:{
+            uint64_t p = Read<uint32_t>() + InTell();
+            ExtendedProperty(ni, offset);
+            InSeek(p);
+            break;}
+        }
+    }
+}
+__declspec(noinline)
+void Img(uint32_t node, uint32_t size) {
+    uint64_t p = InTell();
+    InSkip(1);
+    DeduceKey();
+    InSkip(2);
+    SubProperty(node, p);
+    InSeek(p + size);
+}
+__declspec(noinline)
 void Convert(path filename) {
     OpenInput(filename);
     filename.replace_extension(".nx");
-    assert(Read<uint32_t>() == *reinterpret_cast<uint32_t *>("PKG1"));
+    uint32_t magic = Read<uint32_t>();
+    if (magic != *reinterpret_cast<uint32_t *>("PKG1")) die("Not a valid WZ file");
     InSkip(8);
     FileStart = Read<uint32_t>();
     InSeek(FileStart + 2);
@@ -246,24 +379,29 @@ void Convert(path filename) {
     DeduceKey();
     InSeek(FileStart + 2);
     Nodes.emplace_back();
+    AddString("", 0);
     Directories.emplace(0);
     while (!Directories.empty()) {
         Directory(Directories.front());
         Directories.pop();
     }
     while (!Imgs.empty()) {
-        Img(Imgs.front());
+        Img(Imgs.front().first, Imgs.front().second);
         Imgs.pop();
     }
     CloseInput();
+    cout << Nodes.size() << endl;
+    cout << Strings.size() << endl;
+    cout << StringSet.size() << endl;
     Nodes.clear();
+    Strings.clear();
+    StringSet.clear();
 }
 int main(int argc, char ** argv) {
-    Convert("Skill.wz");
-    //Convert("Data.wz");
+    //Convert("Skill.wz");
+    Convert("Data.wz");
     for (int i = 1; i < argc; ++i) {
         Convert(argv[i]);
     }
-    system("pause");
     return 0;
 }
