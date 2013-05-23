4e52102e3208840709e760c01ee5b27fe22935d5
retep998 <retep998@verizon.net>
2013-05-22 21:49:53 -0400

    Set up NoLifeNx and added AGPLv3
    Signed-off-by: retep998 <retep998@verizon.net>

--- WzToNx.cpp
+++ WzToNx.cpp
@@ -1,3 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+// NoLifeWzToNx - Part of the NoLifeStory project                           //
+// Copyright (C) 2013 Peter Atashian                                        //
+//                                                                          //
+// This program is free software: you can redistribute it and/or modify     //
+// it under the terms of the GNU Affero General Public License as           //
+// published by the Free Software Foundation, either version 3 of the       //
+// License, or (at your option) any later version.                          //
+//                                                                          //
+// This program is distributed in the hope that it will be useful,          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+// GNU Affero General Public License for more details.                      //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public License //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.    //
+//////////////////////////////////////////////////////////////////////////////
 #include <Windows.h>
 #include <filesystem>
 #include <iostream>
@@ -402,7 +419,9 @@ namespace {
             Img(Imgs.front().first, Imgs.front().second);
             Imgs.pop();
         }
+        OpenOutput(filename, 0x100);
         CloseInput();
+        CloseOutput();
     }
 }
 int main(int argc, char ** argv) {
