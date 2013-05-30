9a55988913ed8547d25ad9c61f20a17e9a35e23d
retep998 <retep998@verizon.net>
2013-05-30 01:26:33 -0400

    Horrible map lookup implemented.
    Signed-off-by: retep998 <retep998@verizon.net>

--- NoLifeIRC.cpp
+++ NoLifeIRC.cpp
@@ -28,12 +28,14 @@ namespace NL {
     vector<File> Files;
     Node NXBase, NXCharacter, NXEffect, NXEtc, NXItem, NXMap, NXMob, NXMorph, NXNpc, NXQuest, NXReactor, NXSkill, NXSound, NXString, NXTamingMob, NXUI;
     void Send(string s) {
+        if (s.size() > 510) s.resize(510);
         s += "\r\n";
         Socket.send(s.c_str(), s.length());
     }
-    void Message(string target, string msg) {
-        size_t p = target.find_first_of('!');
-        if (p != target.npos) target.erase(p);
+    void Message(string msg) {
+        string target(Source);
+        size_t p(target.find_first_of('!'));
+        if (p != string::npos) target.erase(p);
         Send("PRIVMSG " + target + " :" + msg);
     }
     void JoinChannel(string channel) {
@@ -44,6 +46,7 @@ namespace NL {
         Stream.get();
     }
     string GetRest() {
+        while (Stream.peek() == ' ') Stream.get();
         string msg;
         getline(Stream, msg);
         return msg;
@@ -67,34 +70,33 @@ namespace NL {
             Stream >> Source;
         }
         function<void(void)> f = IRCCommands[GetNext()];
-        if (!f) {
-            cout << Stream.str() << endl;
-            cout << "Unknown command" << endl;
-        } else f();
+        if (f) f();
     }
     void Connect() {
         Socket.setBlocking(true);
+        Socket.disconnect();
         Socket.connect(Server, Port);
         Socket.setBlocking(false);
         Send("NICK " + Name);
         Send("USER " + Name + " 0 * :" + Name);
+        cout << "Joined server" << endl;
     }
     void Loop() {
         typedef high_resolution_clock Clock;
         string buf;
-        char cbuf[0x1000];
+        char cbuf[0x10000];
         bool pinged = false;
         Clock::time_point last = Clock::now();
         for (;;) {
-            sleep_for(milliseconds(10));
+            sleep_for(milliseconds(100));
             Clock::time_point now = Clock::now();
             size_t r;
-            Socket.receive(cbuf, 0x1000, r);
+            Socket.receive(cbuf, 0x10000, r);
             if (r) {
                 buf.append(cbuf, r);
                 for (;;) {
                     size_t p = buf.find_first_of("\r\n");
-                    if (p == buf.npos) break;
+                    if (p == string::npos) break;
                     try {
                         Stream = istringstream(buf.substr(0, p));
                         Handle();
@@ -109,6 +111,7 @@ namespace NL {
                 Send("PING " + Name);
                 pinged = true;
             } else if (now - last > seconds(20)) {
+                cout << "Timed out" << endl;
                 Connect();
                 last = now;
                 pinged = false;
@@ -118,7 +121,7 @@ namespace NL {
     void HandleKick() {
         string chan = GetNext();
         Send("JOIN " + chan);
-        Message(Source, "I hate you");
+        Message("I hate you");
     }
     void HandlePing() {
         Send("PONG " + GetMessage());
@@ -133,11 +136,30 @@ namespace NL {
                 JoinChannel(chan);
             } else if (com == "lookup") {
                 string type = ToLower(GetNext());
+                function<void(string)> f = LookupCommands[type];
+                if (f) f(GetRest());
             }
         }
     }
     void LookupMap(string s) {
-         
+        Message("Looking up map with Name/ID of " + s);
+        bool found = false;
+        for (Node n1 : NXString["Map.img"]) for (Node n2 : n1) {
+            if (n2.Name().find(s) != string::npos ||
+                string(n2["mapDesc"]).find(s) != string::npos ||
+                string(n2["mapName"]).find(s) != string::npos ||
+                string(n2["streetName"]).find(s) != string::npos) {
+                    found = true;
+                    string result("ID {" + n2.Name() + "}");
+                    if (n2["mapName"]) result += " Name {" + n2["mapName"] + "}";
+                    if (n2["streetName"]) result += " Area {" + n2["streetName"] + "}";
+                    if (n2["mapDesc"]) result += " Description {" + n2["mapDesc"] + "}";
+                    Message(result);
+                    sleep_for(milliseconds(500));
+            }
+        }
+        if (found) Message("End of results");
+        else Message("No results found");
     }
     Node AddFile(char const * s) {
         if (!exists(path(s))) return Node();
@@ -186,25 +208,8 @@ namespace NL {
     }
     void SetupCommands() {
         IRCCommands["KICK"] = HandleKick;
-        IRCCommands["MODE"] = HandleNothing;
-        IRCCommands["NOTICE"] = HandleNothing;
         IRCCommands["PING"] = HandlePing;
-        IRCCommands["PONG"] = HandleNothing;
         IRCCommands["PRIVMSG"] = HandlePrivmsg;
-        IRCCommands["001"] = HandleNothing;
-        IRCCommands["002"] = HandleNothing;
-        IRCCommands["003"] = HandleNothing;
-        IRCCommands["004"] = HandleNothing;
-        IRCCommands["005"] = HandleNothing;
-        IRCCommands["251"] = HandleNothing;
-        IRCCommands["252"] = HandleNothing;
-        IRCCommands["254"] = HandleNothing;
-        IRCCommands["255"] = HandleNothing;
-        IRCCommands["265"] = HandleNothing;
-        IRCCommands["266"] = HandleNothing;
-        IRCCommands["375"] = HandleNothing;
-        IRCCommands["372"] = HandleNothing;
-        IRCCommands["376"] = HandleNothing;
         LookupCommands["map"] = LookupMap;
     }
     void IRCBot() {
