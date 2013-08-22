9a740da64ae54328e6599b0b1425b57c749fadf9
retep998 <retep998@verizon.net>
2013-08-22 14:38:27 -0400

    Cleaned up a tad bit.
    Signed-off-by: retep998 <retep998@verizon.net>

--- nxbench.cpp
+++ nxbench.cpp
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License //
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.    //
 //////////////////////////////////////////////////////////////////////////////
+
 #include <nx/node.hpp>
 #include <nx/file.hpp>
 #include <nx/bitmap.hpp>
@@ -23,84 +24,92 @@
 #include <algorithm>
 #include <numeric>
 #include <cstddef>
+#include <functional>
 #ifdef _WIN32
 #  include <Windows.h>
 #else
 #  include <ctime>
 #endif
-namespace {
-    char const filename[] = "Data.nx";
-    nl::file const file {filename};
-    void load() {
-        nl::file f {filename};
-    }
-    size_t c;
-    void recurse_sub(nl::node n) {
-        ++c;
-        for (nl::node nn : n) recurse_sub(nn);
-    }
-    void recurse_load() {
-        recurse_sub(nl::file(filename));
-    }
-    void recurse() {
-        recurse_sub(file);
-    }
-    void recurse_search_sub(nl::node n) {
-        for (nl::node nn : n) n[nn.name_fast()] == nn ? recurse_search_sub(nn) : throw;
-    }
-    void recurse_search() {
-        recurse_search_sub(file);
-    }
-    void recurse_decompress_sub(nl::node n) {
-        n.get_bitmap().data();
-        for (nl::node nn : n) recurse_decompress_sub(nn);
-    }
-    void recurse_decompress() {
-        recurse_decompress_sub(file);
-    }
+
+namespace nl {
+    namespace {
+        std::string const filename {"Data.nx"};
+        file const nxfile {filename};
+        void load() {
+            file f {filename};
+        }
+        void recurse_sub(node n) {
+            for (node nn : n) recurse_sub(nn);
+        }
+        void recurse_load() {
+            recurse_sub(file(filename));
+        }
+        void recurse() {
+            recurse_sub(nxfile);
+        }
+        void recurse_search_sub(node n) {
+            for (node nn : n) n[nn.name_fast()] == nn ? recurse_search_sub(nn) : throw;
+        }
+        void recurse_search() {
+            recurse_search_sub(nxfile);
+        }
+        void recurse_decompress_sub(node n) {
+            n.get_bitmap().data();
+            for (node nn : n) recurse_decompress_sub(nn);
+        }
+        void recurse_decompress() {
+            recurse_decompress_sub(nxfile);
+        }
 #ifdef _WIN32
-    double frequency;
-    double get_time() {
-        LARGE_INTEGER n;
-        QueryPerformanceCounter(&n);
-        return n.QuadPart * frequency;
-    }
-    void setup_time() {
-        LARGE_INTEGER n;
-        QueryPerformanceFrequency(&n);
-        frequency = 1000000. / n.QuadPart;
-    }
+        double frequency;
+        double get_time() {
+            LARGE_INTEGER n {};
+            QueryPerformanceCounter(&n);
+            return n.QuadPart * frequency;
+        }
+        void setup_time() {
+            LARGE_INTEGER n {};
+            QueryPerformanceFrequency(&n);
+            frequency = 1000000. / n.QuadPart;
+        }
 #else
-    double get_time() {
-        struct timespec t;
-        clock_gettime(CLOCK_MONOTONIC_RAW, &t);
-        return 1000000. * t.tv_sec + 0.001 * t.tv_nsec;
-    }
-    void setup_time() {}
+        double get_time() {
+            struct timespec t;
+            clock_gettime(CLOCK_MONOTONIC_RAW, &t);
+            return 1000000. * t.tv_sec + 0.001 * t.tv_nsec;
+        }
+        void setup_time() {}
 #endif
-    template <typename T>
-    void test(const char * name, T f, size_t maxruns) {
-        f();
-        std::vector<double> results {};
-        do {
-            double const c1 {get_time()};
-            f();
-            double const c2 {get_time()};
-            results.emplace_back(c2 - c1);
-        } while (--maxruns);
-        std::sort(results.begin(), results.end());
-        std::vector<double>::const_iterator n0 {results.cbegin() + static_cast<ptrdiff_t>(results.size()) / 4};
-        std::vector<double>::const_iterator n1 {results.cbegin() + static_cast<ptrdiff_t>(results.size()) * 3 / 4};
-        std::vector<double>::const_iterator n2 {n0 == n1 ? n1 + 1 : n1};
-        std::printf("%s\t%u\t%u\t%u\n", name, static_cast<uint32_t>(*n1), static_cast<uint32_t>(std::accumulate(n0, n2, 0.) / (n2 - n0)), static_cast<uint32_t>(results.front()));
+        void test(std::string name, std::function<void()> func, size_t maxruns) {
+            std::vector<double> results {};
+            do {
+                double const c1 {get_time()};
+                func();
+                double const c2 {get_time()};
+                results.emplace_back(c2 - c1);
+            } while (--maxruns);
+            std::sort(results.begin(), results.end());
+            std::vector<double>::const_iterator const q0 {results.cbegin()};
+            std::vector<double>::const_iterator const q4 {results.cend()};
+            std::vector<double>::const_iterator const q2 {q0 + (q4 - q0) / 2};
+            std::vector<double>::const_iterator const q1 {q0 + (q2 - q0) / 2};
+            std::vector<double>::const_iterator const q3 {q2 + (q4 - q2) / 2};
+            std::printf("%s\t%u\t%u\t%u\n", name.c_str(),
+                        static_cast<unsigned>(*q3),
+                        static_cast<unsigned>(std::accumulate(q1, q3, 0.) / (q3 - q1)),
+                        static_cast<unsigned>(*q0));
+        }
+        void bench() {
+            setup_time();
+            std::printf("Name\t75%%t\tM50%%\tBest\n");
+            test("Ld", load, 0x1000);
+            test("Re", recurse, 0x100);
+            test("LR", recurse_load, 0x100);
+            test("SA", recurse_search, 0x100);
+            test("De", recurse_decompress, 0x10);
+        }
     }
 }
 int main() {
-    setup_time();
-    std::printf("Name\t75%%t\tM50%%\tBest\n");
-    test("Ld", load, 0x1000);
-    test("Re", recurse, 0x100);
-    test("LR", recurse_load, 0x100);
-    test("SA", recurse_search, 0x100);
-    test("De", recurse_decompress, 0x10);
+    nl::bench();
 }
