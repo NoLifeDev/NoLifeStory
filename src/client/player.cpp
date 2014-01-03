0a48f4b3c19ccc94b4d508e250f7527465639ee9
Mika Attila <radiantstatue@gmail.com>
2014-01-03 17:17:53 +0100

    clien/player.cpp: Use new conditional body style

--- src/client/player.cpp
+++ src/client/player.cpp
@@ -32,32 +32,40 @@ namespace nl {
         void respawn(std::string portal) {
             std::vector<std::pair<int32_t, int32_t>> spawns;
             for (auto && p : portals) {
-                if (p.pn == portal) {
+                if (p.pn == portal)
                     spawns.emplace_back(p.x, p.y);
-                }
             }
             if (!spawns.empty()) {
                 auto && spawn = spawns[rand() % spawns.size()];
                 pos.reset(spawn.first, spawn.second - 20);
             } else {
                 std::cerr << "Failed to find portal " << portal << " for map " << map::current_name;
-                if (portal != "sp") respawn("sp");
-                else pos.reset(0, 0);
+                if (portal != "sp")
+                    respawn("sp");
+                else
+                    pos.reset(0, 0);
             }
         }
         void update() {
-            /*if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) pos.left = false;
-            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) pos.right = false;
-            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) pos.up = false;
-            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) pos.down = false;*/
+            /*if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                pos.left = false;
+            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                pos.right = false;
+            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                pos.up = false;
+            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                pos.down = false;*/
             pos.update();
-            if (last_tele + 0.5 > time::delta_total) return;
+            if (last_tele + 0.5 > time::delta_total)
+                return;
             for (portal & p : portals) {
-                if (p.x < pos.x - 40 || p.x > pos.x + 40 || p.y < pos.y - 40 || p.y > pos.y + 40) continue;
+                if (p.x < pos.x - 40 || p.x > pos.x + 40 || p.y < pos.y - 40 || p.y > pos.y + 40)
+                    continue;
                 switch (p.pt) {//Handle stuff like bouncies here
                 case 1:
                 case 2:
-                    if (!pos.up) break;
+                    if (!pos.up)
+                        break;
                 case 3:
                     map::load(std::to_string(p.tm), p.tn);
                     last_tele = time::delta_total;
@@ -66,9 +74,10 @@ namespace nl {
             }
         }
         void render() {
-            //Sprite::Unbind();
-            //if (!config::rave) glColor4f(1, 1, 1, 1);
-            //Graphics::DrawRect(pos.x - 20, pos.y - 60, pos.x + 20, pos.y, true);
+            /*Sprite::Unbind();
+            if (!config::rave)
+                glColor4f(1, 1, 1, 1);
+            Graphics::DrawRect(pos.x - 20, pos.y - 60, pos.x + 20, pos.y, true);*/
         }
     }
 }
