a9f59d265c53821cdea6dede1ceb14f47b8aea35
retep998 <retep998@verizon.net>
2014-01-24 09:16:13 -0500

    Started an NX editing library.
    Signed-off-by: retep998 <retep998@verizon.net>

---
+++ src/nxedit/file.hpp
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////
+// NoLifeNx - Part of the NoLifeStory project                               //
+// Copyright © 2013 Peter Atashian                                          //
+//                                                                          //
+// This program is free software: you can redistribute it and/or modify     //
+// it under the terms of the GNU Affero General Public License as           //
+// published by the Free Software Foundation, either version 3 of the       //
+// License, or (at your option) any later version.                          //
+//                                                                          //
+// This program is distributed in the hope that it will be useful,          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+// GNU Affero General Public License for more details.                      //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public License //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.    //
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include <string>
+#include <vector>
+
+namespace nl {
+    class efile {
+    public:
+        //Please note that this file is kept open until this object is destroyed
+        efile(std::string filename);
+        efile(efile const &) = delete;
+        efile & operator=(efile const &) = delete;
+        ~efile();
+        //Because the loaded file is still open, you have to save to a different file
+        void save(std::string filename);
+    private:
+    };
+}
